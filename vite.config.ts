449f17f76958c60c14aa54ec0c62eb1fd886a795
inkmas <wenyuan_zhao@outlook.com>
2026-04-07 13:26:56 +0800

    init: extra config and dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {viteSingleFile} from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    viteSingleFile(),
   ],
+  build: {
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+
+    rollupOptions: {
+      output: {
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
