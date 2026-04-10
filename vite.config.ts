9937328ccb09e8f5fb8dc4c5f9fe87901d98d500
inkmas <wenyuan_zhao@outlook.com>
2026-04-10 14:56:17 +0800

    init: extra config and dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import {viteSingleFile} from "vite-plugin-singlefile";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     viteSingleFile(),
+    copy({
+      targets: [
+        {
+          src: 'dist/index.html',   // 前端单文件
+          dest: '../downloader_backend/static' // 后端目录
+        }
+      ],
+      hook: 'closeBundle', // 构建完成后执行
+      verbose: true        // 显示复制成功日志
+    })
   ],
   build: {
     assetsInlineLimit: 100000000,
